32a96aa966b3c33fcb7d47c3f00581064d9260c3
fahim-fardin-1045 <fahimfardin1045@gmail.com>
2023-05-26 11:32:25 +0600

    first comment

---
+++ src/learn/Demo.java
@@ -0,0 +1,9 @@
+package learn;
+
+public class Demo {
+    int a= 10;              //////data
+    String b = "fahim";    ////////data
+    void show(){
+        System.out.println( a+"  "+b );
+    }
+}
