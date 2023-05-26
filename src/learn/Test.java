6254f3f0d0b713837ed17d58f46a90fbce743d9f
fahim-fardin-1045 <fahimfardin1045@gmail.com>
2023-05-26 11:39:07 +0600

    first comment

---
+++ src/learn/Test.java
@@ -0,0 +1,9 @@
+package learn;
+
+public class Test {
+    public static void main(String[] args) { ///// main method
+        Demo r = new Demo();  ///////create obj
+        r.show(); /////call method
+    }
+
+}
